d22f29f6da44e19ea7f00547e8f485d40b0c7312
Achref-haddaji <haddaji.achref19@gmail.com>
2022-08-12 16:19:23 +0100

    task2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-# kill a process you must use pkill and exec
-
-exec { 'killmenow':
-  command => 'pkill -f killmenow'
+# kills a process named killmenow using Puppet
+exec {'killmenow':
+path    => '/usr/bin',
+command => 'pkill -f killmenow'
 }
