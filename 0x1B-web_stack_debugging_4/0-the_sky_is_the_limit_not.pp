4a7ae547b57691f5b83144ba4797d4fae964ee99
Achref-haddaji <haddaji.achref19@gmail.com>
2022-10-06 13:20:20 +0100

    task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 #fixe code
 
 exec {'modify max open files limit setting':
-  command => 'sed -i "s/15/9000/" /etc/default/nginx && sudo service nginx restart',
+  command => 'sed -i "s/15/8000/" /etc/default/nginx && sudo service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
